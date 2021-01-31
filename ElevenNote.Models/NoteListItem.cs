685d82cf0ace7391a6540331fa33a7ea97030ba5
unknown <codyamorton2020@gmail.com>
2021-01-31 17:19:43 -0500

    section 4 2nd

--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    class NoteListItem
+    public class NoteListItem
     {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+        
+     
     }
 }
